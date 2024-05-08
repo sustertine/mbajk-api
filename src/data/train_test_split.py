8b23fffb5dcefe7d6b436ece08ac8267e7f99f3f
Tine Šuster <tine.suster@gmail.com>
2024-05-08 20:55:27 +0200

    add files shelf

--- src/data/train_test_split.py
+++ src/data/train_test_split.py
@@ -5,15 +5,7 @@ import pandas as pd
 
 def split_data():
     base_dir = os.getenv('GITHUB_WORKSPACE', '../../')
-    # df = pd.read_csv(f'{base_dir}/data/processed/current_data.csv')
-    #
-    # split = int(len(df) * 0.9)
-    #
-    # train = df.iloc[:split]
-    # test = df.iloc[split:]
-    #
-    # train.to_csv(f'{base_dir}/data/processed/train.csv', index=False)
-    # test.to_csv(f'{base_dir}/data/processed/test.csv', index=False)
+
     for file in os.listdir(f'{base_dir}/data/processed/mbajk'):
         df = pd.read_csv(f'{base_dir}/data/processed/mbajk/{file}')
         split = int(len(df) * 0.9)
@@ -21,11 +13,12 @@ def split_data():
         train = df.iloc[:split]
         test = df.iloc[split:]
 
-        if not os.path.exists(f'{base_dir}/data/processed/{file}'):
-            os.makedirs(f'{base_dir}/data/processed/{file}')
+        filename = os.path.splitext(file)[0]
+        if not os.path.exists(f'{base_dir}/data/processed/train_test/{filename}'):
+            os.makedirs(f'{base_dir}/data/processed/train_test/{filename}')
 
-        train.to_csv(f'{base_dir}/data/processed/{file}/train.csv', index=False)
-        test.to_csv(f'{base_dir}/data/processed/{file}/test.csv', index=False)
+        train.to_csv(f'{base_dir}/data/processed/train_test/{filename}/train.csv', index=False)
+        test.to_csv(f'{base_dir}/data/processed/train_test/{filename}/test.csv', index=False)
 
 
 if __name__ == '__main__':
