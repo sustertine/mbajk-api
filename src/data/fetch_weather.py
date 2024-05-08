8fd65366c95571f3b5598c240545f5745516c5c4
Tine Šuster <tine.suster@gmail.com>
2024-05-08 15:37:18 +0200

    time fetch-weather script

--- src/data/fetch_weather.py
+++ src/data/fetch_weather.py
@@ -2,9 +2,11 @@ import pandas as pd
 import requests
 import os
 from remove_headers import remove_header_rows
+import time
 
 
 def fetch_weather(lat, lng):
+    print(f'Fetching weather for {lat}, {lng}')
     url = (f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lng}1&hourly=temperature_2m,'
            f'relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,rain,surface_pressure')
     response = requests.get(url).json()
@@ -14,6 +16,9 @@ def fetch_weather(lat, lng):
 def fetch_stations_weather():
     base_dir = os.getenv('GITHUB_WORKSPACE', '../../')
     mbajk_dir = os.path.join(base_dir, 'data/raw/mbajk')
+
+    start_time = time.time()
+    print('Fetching weather data (SCRIPT)')
     for filename in os.listdir(mbajk_dir):
         file_path = os.path.join(mbajk_dir, filename)
         df = pd.read_csv(file_path)
@@ -39,7 +44,11 @@ def fetch_stations_weather():
         weather_df.drop_duplicates(subset=['date'], keep='first', inplace=True)
         weather_df.to_csv(weather_file_path, mode='a', index=False)
 
-        remove_header_rows(f'{base_dir}/data/raw/weather/')
+    print(f'Finished fetching weather data (SCRIPT), time: {time.time() - start_time}s')
+    start_time = time.time()
+    print('Removing headers from weather data (SCRIPT)')
+    remove_header_rows(f'{base_dir}/data/raw/weather/')
+    print(f'Finished headers from weather data (SCRIPT), time: {time.time() - start_time}s')
 
 
 if __name__ == '__main__':
