42cffc20244f3373cd8f08f7d46a496762eca3cf
Tine Šuster <tine.suster@gmail.com>
2024-05-09 15:02:15 +0200

    refactor

--- src/models/train_eval_models.py
+++ src/models/train_eval_models.py
@@ -9,16 +9,56 @@ from keras.layers import LSTM, Dense
 from keras.losses import MeanSquaredError, MeanAbsoluteError
 from keras.optimizers import Adam
 import mlflow
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from  src.models.feature_engineer import FeatureEngineer
+from sklearn import set_config
+
+set_config(transform_output="pandas")
 
 load_dotenv()
 mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
 username = os.getenv('MLFLOW_TRACKING_USERNAME')
 password = os.getenv('MLFLOW_TRACKING_PASSWORD')
+
+
+class FeatureEngineer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+
+        X['date'] = pd.to_datetime(X['date'])
+        X.sort_values(by='date', inplace=True)
+        X['time_of_day'] = X['date'].dt.hour // 6
+
+        X['day_of_week'] = X['date'].dt.dayofweek
+        X = pd.get_dummies(X, columns=['time_of_day', 'day_of_week'], drop_first=True)
+
+        X['lagged_available_bike_stands'] = X['available_bike_stands'].shift(1)
+
+        window_size = 7
+        X['rolling_mean_bike_stands'] = X['available_bike_stands'].rolling(window=window_size).mean()
+
+        X['rolling_std_bike_stands'] = X['available_bike_stands'].rolling(window=window_size).std()
+
+        X['diff_available_bike_stands'] = X['available_bike_stands'].diff()
+
+        X['temperature_diff'] = X['temperature'] - X['apparent_temperature']
+
+        X = X[['temperature', 'dew_point', 'apparent_temperature',
+               'surface_pressure', 'available_bike_stands',
+               'lagged_available_bike_stands', 'rolling_mean_bike_stands',
+               'rolling_std_bike_stands', 'diff_available_bike_stands',
+               'temperature_diff']]
+
+        return X
 
 
 def create_dataset(dataset, look_back=1, look_forward=5):
@@ -65,6 +105,7 @@ def preprocess_data(df):
 
     return out_df
 
+
 def train_eval_model(train_file_path, test_file_path):
     station_name = os.path.basename(os.path.dirname(train_file_path))
 
@@ -100,6 +141,7 @@ def train_eval_model(train_file_path, test_file_path):
 
         history = model.fit(trainX, trainY, epochs=n_epochs, batch_size=batch_size, verbose=1)
         evaluation = model.evaluate(testX, testY, verbose=1)
+
 
 def train_eval_models():
     base_dir = os.getenv('GITHUB_WORKSPACE', '../../')
